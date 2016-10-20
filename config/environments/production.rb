f739b3c40559156a18126f72cdd6585b347e45ac
Greg Rook <gar5039@gmail.com>
2016-10-19 20:05:58 -0400

    add login links in navbar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'https://nomster-greg-rook.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'nomster-greg-rook.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
